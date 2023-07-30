51710587d11e2f9a4f90dfb3acaacfac0b0d5579
alexander-neumeier <63714415+alexander-neumeier@users.noreply.github.com>
2023-07-30 22:03:04 +0200

    Fixed some Stuff

--- src/main/java/at/alex/homes/commands/DelHome.java
+++ src/main/java/at/alex/homes/commands/DelHome.java
@@ -23,11 +23,10 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
-import at.alex.homes.utils.configManager;
+
 
 public class DelHome implements CommandExecutor {
     FileHandler fileHandler = new FileHandler();
-    configManager savedconfig = new configManager();
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         Player player = (Player) sender;
